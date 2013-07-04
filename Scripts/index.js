133b84e9e5d22673da8899ae9b32fdf7971880e0
Erik Gaius Capistrano <gaiussensei@gmail.com>
2013-07-04 02:50:04 -0400

    remove friesJs, start on UI of index.

---
+++ Scripts/index.js
@@ -0,0 +1,16 @@
+"use strict";
+document.addEventListener("deviceready", onReady, false);
+var onReady = function onReadyF() {
+    // Initialize Phonegap Stuff
+    // 
+};
+head.js(
+    { pg: "phonegap.js" },
+    { jq: "Scripts/jQuery/jquery-1.9.1.min.js" },
+    { ts: "Scripts/jQuery/jquery.total-storage.min.js" },
+    { ui: "Scripts/Bootstrap/bootstrap.min.js" },
+    function readyF() {
+        // Initialize App
+        // 
+    }
+);
